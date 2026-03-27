717fe274604f0fe9b09d83c9008c190d328adfc6
HermesCui <107318463+HermesCui@users.noreply.github.com>
2026-03-27 18:54:30 +0000

    Deploying to gh-pages from @ HermesCui/HermesCui.github.io@d9c625d94a387455c886a039e40f69ceffb9902a 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "HermesCui/ChihanCui.github.io",
+    "data-repo": "HermesCui/HermesCui.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
