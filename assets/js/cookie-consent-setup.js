78816120e8115c179e2d7aed8ccf16d01bf5bcd0
HermesCui <107318463+HermesCui@users.noreply.github.com>
2026-03-27 22:08:05 +0000

    Deploying to gh-pages from @ HermesCui/HermesCui.github.io@f5a707aa5a625968370a3daeed6a106652ca2244 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://hermescui.github.io/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://chihancui.com/#contact">contact us</a>.'
               }
             ]
           }
